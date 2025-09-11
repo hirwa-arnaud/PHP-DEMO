0f06a5d566d7e7b74b145c0beac3674bcd2eff36
Hirwa Arrnaud <hirwatrevor8@gmail.com>
2025-09-11 17:20:36 +0200

    finished area

---
+++ Area of shapes/area.php
@@ -0,0 +1,10 @@
+<?php
+$p=90000;
+$r=4;
+$t=2;
+$interest=($p*$r*$t)/100;
+echo "The Principal is ${p}";
+echo "The rate is ${r}";
+echo "The time is ${t}";
+echo "The Simple interest ${interest}";
+?>
