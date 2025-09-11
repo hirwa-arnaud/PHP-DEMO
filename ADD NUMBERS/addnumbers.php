10fe900b8a30154af2577b6babab7e185fa22fd5
Hirwa Arrnaud <hirwatrevor8@gmail.com>
2025-09-11 16:26:55 +0200

    added rules

--- ADD NUMBERS/addnumbers.php
+++ ADD NUMBERS/addnumbers.php
@@ -1,7 +1,10 @@
 <?php
 $a=30;
 $b=25;
+//there is no space btn variables
 //variable cant start with number
+//variable should start with dollar sign($)
+//variable name is case sensitive
 $sum=$a+$b;
 echo"Sum is $sum";
 ?>
